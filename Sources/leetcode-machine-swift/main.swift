b1655210b252a11e869738f2f5c68a3cbad546e4
Hao <changhao@sendbest.cc>
2017-05-07 08:50:45 +0800

    fix wrong problem

--- Sources/leetcode-machine-swift/main.swift
+++ Sources/leetcode-machine-swift/main.swift
@@ -11,7 +11,7 @@ import Foundation
 // set problems
 Machine.problems = [
     1: Question(name: "Two Sum", inputs: [([2,7,11,15], 9)], outputs: [[0, 1]], validation: { return compare($0, $1) }),
-    476 : Question(name: "Number Complement", inputs: [5, 1], outputs: [2, 10]),
+    476 : Question(name: "Number Complement", inputs: [5, 1], outputs: [2, 0]),
     561 : Question(name: "Array Partition I", inputs: [[1,4,3,2]], outputs: [4])
 ]
 
